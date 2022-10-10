6a834269e018397964ba07f6312478fca8f0a6e5
Danilo Maximiano <99772416+Danmqest@users.noreply.github.com>
2022-10-10 20:48:01 -0300

    Add files via upload

---
+++ pedido.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.projetointergador2;
+
+/**
+ *
+ * @author Nusa
+ */
+public class pedido {
+    private int idPedido;
+    private ;
+    
+    
+}
