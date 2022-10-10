e571c48022d9a3a9ca47ca9a5339ac5f71b4e666
Danilo Maximiano <99772416+Danmqest@users.noreply.github.com>
2022-10-10 20:38:03 -0300

    Add files via upload

---
+++ Cliente.java
@@ -0,0 +1,100 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.projetointergador2;
+
+/**
+ *
+ * @author Nusa
+ */
+public class Cliente {
+    
+    private int idCliente;
+    private String nomeCli;
+    private int cpfCli;
+    private String enderecoCli;
+    private int telefoneCli;
+    private String emailCli;
+    private int sexoCli;
+    private int estCivilCli;
+    private int dataNascimentoCli;
+    
+    public Cliente(){
+        
+    }
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getNomeCli() {
+        return nomeCli;
+    }
+
+    public void setNomeCli(String nomeCli) {
+        this.nomeCli = nomeCli;
+    }
+
+    public int getCpfCli() {
+        return cpfCli;
+    }
+
+    public void setCpfCli(int cpfCli) {
+        this.cpfCli = cpfCli;
+    }
+
+    public String getEnderecoCli() {
+        return enderecoCli;
+    }
+
+    public void setEnderecoCli(String enderecoCli) {
+        this.enderecoCli = enderecoCli;
+    }
+
+    public int getTelefoneCli() {
+        return telefoneCli;
+    }
+
+    public void setTelefoneCli(int telefoneCli) {
+        this.telefoneCli = telefoneCli;
+    }
+
+    public String getEmailCli() {
+        return emailCli;
+    }
+
+    public void setEmailCli(String emailCli) {
+        this.emailCli = emailCli;
+    }
+
+    public int getSexoCli() {
+        return sexoCli;
+    }
+
+    public void setSexoCli(int sexoCli) {
+        this.sexoCli = sexoCli;
+    }
+
+    public int getEstCivilCli() {
+        return estCivilCli;
+    }
+
+    public void setEstCivilCli(int estCivilCli) {
+        this.estCivilCli = estCivilCli;
+    }
+
+    public int getDataNascimentoCli() {
+        return dataNascimentoCli;
+    }
+
+    public void setDataNascimentoCli(int dataNascimentoCli) {
+        this.dataNascimentoCli = dataNascimentoCli;
+    }
+    
+    
+}
