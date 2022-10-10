8e268f6699d1a98316911599d2eebb663739cf85
Danilo Maximiano <99772416+Danmqest@users.noreply.github.com>
2022-10-10 20:38:42 -0300

    Add files via upload

---
+++ produto.java
@@ -0,0 +1,84 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.projetointergador2;
+
+/**
+ *
+ * @author Nusa
+ */
+public class produto {
+     
+    private int idProduto;
+    private String nomeProduto;
+    private int valorProduto;
+    private int tamanhoProduto;
+    private int corProduto;
+    private int generoProduto;
+    private int quantidadeProduto;
+    
+    public produto(){
+        
+    }
+
+    public int getIdProduto() {
+        return idProduto;
+    }
+
+    public void setIdProduto(int idProduto) {
+        this.idProduto = idProduto;
+    }
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public int getValorProduto() {
+        return valorProduto;
+    }
+
+    public void setValorProduto(int valorProduto) {
+        this.valorProduto = valorProduto;
+    }
+
+    public int getTamanhoProduto() {
+        return tamanhoProduto;
+    }
+
+    public void setTamanhoProduto(int tamanhoProduto) {
+        this.tamanhoProduto = tamanhoProduto;
+    }
+
+    public int getCorProduto() {
+        return corProduto;
+    }
+
+    public void setCorProduto(int corProduto) {
+        this.corProduto = corProduto;
+    }
+
+    public int getGeneroProduto() {
+        return generoProduto;
+    }
+
+    public void setGeneroProduto(int generoProduto) {
+        this.generoProduto = generoProduto;
+    }
+
+    public int getQuantidadeProduto() {
+        return quantidadeProduto;
+    }
+
+    public void setQuantidadeProduto(int quantidadeProduto) {
+        this.quantidadeProduto = quantidadeProduto;
+    }
+    
+    
+    
+    
+}
